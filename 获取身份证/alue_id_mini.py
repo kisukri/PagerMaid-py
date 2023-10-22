8a4d66539a280ec265893129166dd6720228f0ad
kisukri <134156879+kisukri@users.noreply.github.com>
2023-10-22 11:23:07 +0800

    Add files via upload

---
+++ 获取身份证/alue_id_mini.py
@@ -0,0 +1,30 @@
+#Alue制作，请勿用于非法用途
+from pagermaid.listener import listener
+from pagermaid.enums import AsyncClient, Message
+from pagermaid.utils import lang
+
+import requests
+
+@listener(command="sfz",
+          description=lang("随机获取一个真实有效的身份证信息(cn/tw)"))
+async def sfz(request: AsyncClient, message: Message):
+    args = message.text.split()
+    if len(args) > 1:
+        country = args[1]
+        if country in ['cn', 'tw']:
+            url = f"https://api.aagtool.top/api/sjsfz?country={country}"
+            response = await request.get(url)
+            if response.status_code == 200:
+                data = response.json()
+                if data["code"] == "0":
+                    result = {"user": data["user"], "userid": data["userid"]}
+                    info = f"{lang('姓名')}：{result['user']}\n{lang('身份证号')}：{result['userid']}"
+                    await message.edit(info)
+                else:
+                    await message.edit(lang("获取失败，请重试"))
+            else:
+                await message.edit(lang("请求接口失败", str(response.status_code)))
+        else:
+            await message.edit(lang("错误的地区"))
+    else:
+        await message.edit(lang("请指定地区"))
